11f73d51446d66014ffc8bdef31f75d7223b0777
anthony <quereantho@gmail.com>
2022-10-10 13:33:07 +0200

    :bug: Fix dependency injection

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -7,43 +7,9 @@ using System.Threading.Channels;
 using Leonardo;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-
-
-var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
-var dataBaseName = Guid.NewGuid().ToString();
-builder.UseInMemoryDatabase(dataBaseName);
-
-var options = builder.Options;
-var fibonacciDataContext = new FibonacciDataContext(options);
-await fibonacciDataContext.Database.EnsureCreatedAsync(); 
-
-Console.WriteLine("Hello, World!");
-
-var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-    
-IConfiguration configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
-    .Build();
-    
-var applicationSection = configuration.GetSection("Application");
-var applicationConfig = applicationSection.Get<ApplicationConfig>();
-
-var loggerFactory = LoggerFactory.Create(builder => {
-        builder.AddFilter("Microsoft", LogLevel.Warning)
-            .AddFilter("System", LogLevel.Warning)
-            .AddFilter("Demo", LogLevel.Debug)
-            .AddConsole();
-    }
-);
-var logger = loggerFactory.CreateLogger("Demo.Program");
-
-logger.LogInformation($"Application Name : {applicationConfig.Name}");
-logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
 T ControlTime<T>(Func<T> a)
 {
@@ -60,17 +26,63 @@ T ControlTime<T>(Func<T> a)
 }
 
 
-var a = await new Fibonacci(fibonacciDataContext).RunAsync(args);
 
 
+var services = new ServiceCollection();
 
-Console.WriteLine($"res: {a.Select(a  => a.ToString()).Aggregate((aaa, bbb) => $"{aaa}, {bbb}")}");
+services.AddSingleton(implementationFactory: _ =>
+{
+    var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+    var dataBaseName = Guid.NewGuid().ToString();
+    builder.UseInMemoryDatabase(dataBaseName);
+    var options = builder.Options;
+
+    return new FibonacciDataContext(options);
+});
+services.AddTransient<Fibonacci>();
+services.AddLogging(configure => configure.AddConsole());
+
+await using var serviceProvider = services.BuildServiceProvider();
+{
+    var fibonacciDataContext = serviceProvider.GetService<FibonacciDataContext>();
+    await fibonacciDataContext.Database.EnsureCreatedAsync();
+
+    Console.WriteLine("Hello, World!");
+
+    var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddEnvironmentVariables()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+        .Build();
+
+    var applicationSection = configuration.GetSection("Application");
+    var applicationConfig = applicationSection.Get<ApplicationConfig>();
+
+    var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("Demo", LogLevel.Debug)
+                .AddConsole();
+        }
+    );
+    var logger = loggerFactory.CreateLogger("Demo.Program");
+
+    logger.LogInformation($"Application Name : {applicationConfig.Name}");
+    logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
 
+    var a = await serviceProvider.GetService<Fibonacci>().RunAsync(args);
+
+
+    Console.WriteLine($"res: {a.Select(a => a.ToString()).Aggregate((aaa, bbb) => $"{aaa}, {bbb}")}");
+}
 
 public class ApplicationConfig
 {
     public string Name { get; set; }
     public string Message { get; set; }
 }
-
--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,9 +1,29 @@
 using Leonardo;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/fibonacci", () => Fibonacci.RunAsync(new[] { "44", "45" }));
+var services = new ServiceCollection();
 
-app.Run();
+services.AddSingleton(implementationFactory: _ =>
+{
+    var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+    var dataBaseName = Guid.NewGuid().ToString();
+    builder.UseInMemoryDatabase(dataBaseName);
+    var options = builder.Options;
+
+    return new FibonacciDataContext(options);
+});
+services.AddTransient<Fibonacci>();
+services.AddLogging(configure => configure.AddConsole());
+
+await using var serviceProvider = services.BuildServiceProvider();
+{
+
+    app.MapGet("/fibonacci", () => serviceProvider.GetService<Fibonacci>().RunAsync(new[] { "44", "45" }));
+
+    app.Run();
+
+}
